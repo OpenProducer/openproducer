7f32c36e756c69ca574a5406cfc399ff433eb3c3
Gus Austin <gusaus@gmail.com>
2023-03-10 19:06:58 -0800

    newspack release updates

---
+++ wp-content/plugins/newspack-listings/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'automattic/newspack-listings',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '66ac5312e72073fe4595e3066e38c3dcad358508',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/newspack-listings' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '66ac5312e72073fe4595e3066e38c3dcad358508',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ wp-content/plugins/newspack-sponsors/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'automattic/newspack-new-plugin-boilerplate',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6277f6b4edd8a000ef0a362205c76386894c162c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'automattic/newspack-new-plugin-boilerplate' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6277f6b4edd8a000ef0a362205c76386894c162c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
