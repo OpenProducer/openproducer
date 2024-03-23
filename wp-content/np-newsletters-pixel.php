2fcf726b58e5e71a1199057f8fde04f9243a5122
Gus Austin <gusaus@gmail.com>
2024-03-23 19:41:38 +0000

    updates via dashbaord

--- wp-content/np-newsletters-pixel.php
+++ wp-content/np-newsletters-pixel.php
@@ -17,7 +17,7 @@
 		if ( ! isset( $_GET["id"] ) || ! isset( $_GET["tid"] ) || ! isset( $_GET["em"] ) ) {
 			exit;
 		}
-		$file = "/files/2024/02/newspack_newsletters_pixel_log_lyMlJO";
+		$file = "/files/2024/03/newspack_newsletters_pixel_log_6arMlB";
 		$id = $_GET["id"];
 		$tid = $_GET["tid"];
 		$email_address = $_GET["em"];
