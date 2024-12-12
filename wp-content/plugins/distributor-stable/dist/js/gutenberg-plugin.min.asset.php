88fe507880e6b07a5428ab56fdd046c8eb275937
Gus Austin <gusaus@gmail.com>
2024-12-12 00:14:32 +0000

    updates via dashboard

--- wp-content/plugins/distributor-stable/dist/js/gutenberg-plugin.min.asset.php
+++ wp-content/plugins/distributor-stable/dist/js/gutenberg-plugin.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives'), 'version' => '97ad6470ab1749788a36');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-primitives'), 'version' => '9c07052aef2738354368');
