4f73f0bae6d5b2741badbbb8045801ea2ef15fef
Gus Austin <gusaus@gmail.com>
2026-08-28 20:20:16 +0000

    updates via dashboard

--- wp-content/plugins/woocommerce-gateway-stripe/vendor/composer/installed.php
+++ wp-content/plugins/woocommerce-gateway-stripe/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'woocommerce/woocommerce-gateway-stripe',
-        'pretty_version' => 'dev-release/10.8.4',
-        'version' => 'dev-release/10.8.4',
-        'reference' => 'c9ec847273042aa3bcda014fcf05e717b35627a2',
+        'pretty_version' => 'dev-release/10.9.0',
+        'version' => 'dev-release/10.9.0',
+        'reference' => '836a0cfe6a6f2e574d1d8dd8e6604a8ad1428bbe',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'woocommerce/woocommerce-gateway-stripe' => array(
-            'pretty_version' => 'dev-release/10.8.4',
-            'version' => 'dev-release/10.8.4',
-            'reference' => 'c9ec847273042aa3bcda014fcf05e717b35627a2',
+            'pretty_version' => 'dev-release/10.9.0',
+            'version' => 'dev-release/10.9.0',
+            'reference' => '836a0cfe6a6f2e574d1d8dd8e6604a8ad1428bbe',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
